d2784a1f943bafda616861b726344078abc585e0
Samson Nguyen <samson.nguyen@solium.com>
2015-04-04 12:12:57 -0600

    Jenv enhancements - Install jenv ant plugin and ensure SQLDeveloper runs with 1.7

---
+++ manifests/jenv.pp
@@ -0,0 +1,19 @@
+## Installs jenv and set's up various components
+class solium::jenv( $user = undef,
+                    $home = undef) {
+
+  validate_string($user,
+                  $home)
+
+  package { 'jenv':
+    ensure  => installed,
+  }
+
+  exec { 'install jenv ant plugin':
+    command => 'jenv enable-plugin ant',
+    user    => $user,
+    creates => "${home}/.jenv/shims/ant",
+    require => Package['jenv']
+  }
+
+}
--- manifests/sqldeveloper.pp
+++ manifests/sqldeveloper.pp
@@ -1,7 +1,11 @@
 # Install sqldeveloper
 class solium::sqldeveloper( $version = '4.0.3.16.84',
+                            $user    = undef,
+                            $group   = undef,
                             $host    = undef) {
   validate_string($host,
+                  $user,
+                  $group,
                   $version)
 
   package { 'sqldeveloper':
@@ -9,5 +13,12 @@ class solium::sqldeveloper( $version = '4.0.3.16.84',
     flavor   => 'zip',
     provider => 'compressed_app',
     source   => "${host}/sqldeveloper-${version}-macosx.app.zip"
+  }
+
+  file { '/Applications/SQLDeveloper.app/.java-version':
+    owner   => $user,
+    group   => $group,
+    content => template('solium/sqldeveloper/.java-version.erb'),
+    require => [ Package['jenv'],Package['sqldeveloper'] ],
   }
 }
